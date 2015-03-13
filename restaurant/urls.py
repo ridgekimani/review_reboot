3f91e2370a58cb0b2ac4cbfd074948efade74a39
miznat <apps.miznat@gmail.com>
2015-03-14 01:43:45 +0800

    another hotfix

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -36,7 +36,7 @@ urlpatterns = patterns('',
 )
 
 if settings.DEBUG:
-    urlpatterns = patterns('',
+    urlpatterns += patterns('',
         url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
             {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
         url(r'', include('django.contrib.staticfiles.urls')),
