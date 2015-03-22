e8a1d1c0f7b5e7dc59a4fd4f2bbc7f48af017fd1
melnychukk <melnychukk18@gmail.com>
2015-03-22 17:42:09 +0100

    Google social auth

--- restaurant/settings/social.py
+++ restaurant/settings/social.py
@@ -4,6 +4,9 @@ from restaurant.settings import credentials
 SOCIAL_AUTH_FACEBOOK_KEY = credentials['SOCIAL_AUTH_FACEBOOK_KEY']
 SOCIAL_AUTH_FACEBOOK_SECRET = credentials['SOCIAL_AUTH_FACEBOOK_SECRET']
 
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '367907061378-gg521bdfualdo10ssqt6k22n7lbtdmsf.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'lTrAse3L_EJNYLKICzlDQsjp'
+
 # SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 
 # for more backends check http://django-social-auth.readthedocs.org/en/latest/configuration.html
@@ -11,6 +14,7 @@ AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
     "social.backends.facebook.FacebookOAuth2",
     "social.backends.google.GoogleOpenIdConnect",
+    'social.backends.google.GoogleOAuth2'
 )
 
 
@@ -25,7 +29,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social.pipeline.social_auth.associate_user',
     'social.pipeline.social_auth.load_extra_data',
     'social.pipeline.user.user_details',
-    'venues.pipeline.social_profile_link'
+    'venues.pipeline.social_profile_link',
 )
 
 
