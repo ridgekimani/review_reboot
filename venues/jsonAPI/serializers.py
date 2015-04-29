2c9df52c14b461f5a1717136a7f22eb758ba3b2c
abomariam <abo_mariam1@hotmail.com>
2015-04-29 14:46:02 +0200

    Add note_count to the JSON Resoponse

--- venues/jsonAPI/serializers.py
+++ venues/jsonAPI/serializers.py
@@ -1,16 +1,27 @@
 from django.core import serializers
+from venues.models.note import Note
 
+import json
 
 def RestaurantSerializer(format, queryset, **options):
 
     MENU_TYPES = {0:"?",1:"Partially Halal",2: "Full Halal" }
 
     CHOICES = {None: "?",False: "No",True: "Yes"}
-
+    obj_list = []
     for obj in queryset:
         obj.menu = MENU_TYPES[obj.menu]
         obj.porkFree = CHOICES[obj.porkFree]
         obj.catering = CHOICES[obj.catering]
         obj.muslimOwner = CHOICES[obj.muslimOwner]
 
-    return serializers.serialize(format,queryset, **options)
+
+        json_obj = serializers.serialize(format,(obj,),**options)
+        obj_json = json.loads(json_obj)
+        obj_json[0]['fields'].update({'note_count': len(Note.list_for_venue(obj))})
+
+        obj_list.append(obj_json[0])
+
+    json_return = json.dumps(obj_list)
+
+    return json_return
