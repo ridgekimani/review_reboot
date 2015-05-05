a8b2591a766a93a104523df03916e4b7e4e44b6a
abomariam <Eng.Mahmoud.A.Mostafa@gmail.com>
2015-05-05 21:21:04 +0200

    fix the Sect name bug

--- importMasjid.py
+++ importMasjid.py
@@ -9,10 +9,8 @@ from venues.models.sect import Sect
 
 counter = 0
 
-sq_conn = sqlite3.connect('/home/miznat/masjid.db')
+sq_conn = sqlite3.connect('masjid.db')
 sq_curs = sq_conn.cursor()
-
-
 
 for row in sq_curs.execute('SELECT * FROM Masjid;'):
     counter += 1
@@ -21,23 +19,19 @@ for row in sq_curs.execute('SELECT * FROM Masjid;'):
         sect = Sect.objects.get(name=sect_name)
     except:
         sect = Sect.objects.create(name=sect_name)
-
-
-    masjid = Masjid(
-        name=row[0],
-        address = row[1],
-        sect = sect,
-        location = geos.GEOSGeometry('POINT(%s %s)' %(row[4], row[3])),
-        city = row[5],
-        country = row[6])
+    masjid = Masjid(name=row[0],
+        address=row[1],
+        sect=sect,
+        location=geos.GEOSGeometry('POINT(%s %s)' % (row[4], row[3])),
+        city=row[5],
+        country=row[6])
     masjid.save()
-
 
 print "Saved in db: ", counter
 sq_conn.close()
 
 # TABLE Masjid (
-#     name text, 
+# name text,
 #     latitude int, 
 #     longitude int
 # );
