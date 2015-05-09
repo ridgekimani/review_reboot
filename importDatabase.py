fda38fc8e32f27db0437fd16cce0664f19b5dcc1
abomariam <Eng.Mahmoud.A.Mostafa@gmail.com>
2015-05-09 07:33:52 +0200

    update the importDatabse.py and Restaurant Model to contain the full details

--- importDatabase.py
+++ importDatabase.py
@@ -8,7 +8,8 @@ import sqlite3
 from django.contrib.gis import geos
 from venues.models import Cuisine, Restaurant
 
-RESTS_SQLITE_FILE = '/home/miznat/NewUpload.db'
+# RESTS_SQLITE_FILE = '/home/miznat/NewUpload.db'
+RESTS_SQLITE_FILE = 'restaurant.db'
 
 f = open('cuisineList.txt', 'r')
 cats = [c.replace("\n", "") for c in f.readlines()]
@@ -25,15 +26,20 @@ for row in sq_curs.execute("SELECT * FROM Restaurant;"):
         name=row[0],
         address=row[1],
         phone=row[2].replace(" ", "") or '',
-        catering="Catering" in row[4],
+        catering="Catering" in row[3],
         delivery="Delivery" in row[4],
-        location=geos.GEOSGeometry('POINT(%s %s)' % (row[10], row[9])),
+        yelp_id = row[5] or '',
+
         yelp_url=row[6] or '',
+
+        foursquare_id=row[7] or '',
         foursquare_url=row[8] or '',
-        approved=True,
+        location=geos.GEOSGeometry('POINT(%s %s)' % (row[10], row[9])),
         city=row[11],
         country=row[12],
-        website=row[13],
+        website=row[13] or '',
+        approved=True,
+
     )
     rest.save()
     counter += 1
